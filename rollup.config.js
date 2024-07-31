49a274cc4d436f6039e72b1e79b58de1fac8c27e
WellDoItToo <welldoittoo@gmail.com>
2024-07-31 18:03:39 +0300

    version 1.0.1

--- rollup.config.js
+++ rollup.config.js
@@ -6,46 +6,44 @@ import { terser } from 'rollup-plugin-terser';
 import path from 'path';
 
 export default {
-    input: 'src/index.ts',
-    output: [
-        {
-            file: 'dist/index.js',
-            format: 'umd',
-            name: 'tsWebsocketClient',
-            sourcemap: true
-        },
-        {
-            file: 'dist/index.min.js',
-            format: 'umd',
-            name: 'tsWebsocketClient',
-            sourcemap: true,
-            plugins: [terser()]
-        },
-        {
-            file: 'dist/index.esm.js',
-            format: 'es',
-            sourcemap: true
-        },
-        {
-            file: 'dist/index.esm.min.js',
-            format: 'es',
-            sourcemap: true,
-            plugins: [terser()]
-        }
-    ],
-    plugins: [
-        alias({
-            entries: [
-                { find: '@', replacement: path.resolve(__dirname, 'src') }
-            ]
-        }),
-        resolve(),
-        commonjs(),
-        typescript({
-            tsconfig: './tsconfig.json',
-            declaration: true,
-            declarationDir: 'dist',
-            rootDir: 'src'
-        })
-    ]
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'umd',
+      name: 'tsWebsocketClient',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.min.js',
+      format: 'umd',
+      name: 'tsWebsocketClient',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+    {
+      file: 'dist/index.esm.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.esm.min.js',
+      format: 'es',
+      sourcemap: true,
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    alias({
+      entries: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
+    }),
+    resolve(),
+    commonjs(),
+    typescript({
+      tsconfig: './tsconfig.json',
+      declaration: true,
+      declarationDir: 'dist',
+      rootDir: 'src',
+    }),
+  ],
 };
